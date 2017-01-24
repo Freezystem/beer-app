ddcd3f701f52c32981ae34a3d71544a22c7eb64d
Freezystem <freezystem@gmail.com>
2017-01-24 14:12:25 +0100

    add beer details in /beers/:id route

--- src/components/BeerDetails/index.js
+++ src/components/BeerDetails/index.js
@@ -1,18 +1,41 @@
-// @flow
+import React        from 'react';
+import { connect }  from 'react-redux';
 
-import React, {
-  Component
-}                 from 'react';
+const BeerDetails = ({ beer }) => {
+  let { name, first_brewed, tagline, description, food_pairing, brewers_tips, ingredients } = beer;
+  let { hops } = ingredients;
 
-class BeerDetails extends Component {
-  constructor() {
-    super();
-    console.log('state', this.state);
-  }
+  return (
+    <div>
+      <h2>{name} - {first_brewed}</h2>
+      <h3>{tagline}</h3>
+      <p>{description}</p>
+      <p>{brewers_tips}</p>
+      <div>
+        <p>food pairing:</p>
+        <ul>
+          {Array.isArray(food_pairing) && food_pairing.map(( pair, id ) => <li key={id}>{pair}</li>)}
+        </ul>
+      </div>
+      <div>
+        <p>hops:</p>
+        <ul>
+          {Array.isArray(hops) && hops.map(( hop, id ) => <li key={id}>{hop.name} ({hop.amount.value}{hop.amount.unit})</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  render() {
-    return (<div>beer details : {this.props.params.id}</div>);
-  }
-}
+const getBeer = (beers = [], id) => {
+  let findBeer = Array.isArray(beers) && beers.filter(beer => beer.id === id);
+  return findBeer.length ? findBeer[0] : {};
+};
 
-export default BeerDetails;
+const MapStateToProps = (state, props) => ({
+  beer : getBeer(state.beers.data, parseInt(props.params.id, 10))
+});
+
+export default connect(
+  MapStateToProps
+)(BeerDetails);
