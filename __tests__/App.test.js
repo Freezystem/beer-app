fc5b1921f7a3faae7dccac9467d9e114d10a3ef8
Freezystem <freezystem@gmail.com>
2017-01-27 11:31:48 +0100

    fix base test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,10 +1,13 @@
 // @flow
 
-import React      from 'react';
-import ReactDOM   from 'react-dom';
-import App        from '../src/modules/BeerApp';
+import React          from 'react';
+import ReactDOM       from 'react-dom';
+import Root           from '../src/components/Root';
+import configureStore from '../src/helpers/configureStore';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('Beer App', () => {
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Root store={configureStore()}/>, div);
+  });
 });
